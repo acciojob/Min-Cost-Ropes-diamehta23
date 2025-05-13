31d87956d8878c898aa959de145251ec3579f985
diamehta23 <154620279+diamehta23@users.noreply.github.com>
2025-05-13 21:20:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,86 @@
-function mincost(arr)
-{ 
-//write your code here
-// return the min cost
-  
+class MinHeap {
+    constructor() {
+        this.heap = [];
+    }
+
+    insert(val) {
+        this.heap.push(val);
+        this._bubbleUp();
+    }
+
+    extractMin() {
+        if (this.heap.length === 1) return this.heap.pop();
+        const min = this.heap[0];
+        this.heap[0] = this.heap.pop();
+        this._sinkDown();
+        return min;
+    }
+
+    size() {
+        return this.heap.length;
+    }
+
+    _bubbleUp() {
+        let idx = this.heap.length - 1;
+        const element = this.heap[idx];
+        while (idx > 0) {
+            let parentIdx = Math.floor((idx - 1) / 2);
+            let parent = this.heap[parentIdx];
+            if (element >= parent) break;
+            this.heap[parentIdx] = element;
+            this.heap[idx] = parent;
+            idx = parentIdx;
+        }
+    }
+
+    _sinkDown() {
+        let idx = 0;
+        const length = this.heap.length;
+        const element = this.heap[0];
+
+        while (true) {
+            let leftIdx = 2 * idx + 1;
+            let rightIdx = 2 * idx + 2;
+            let swap = null;
+
+            if (leftIdx < length) {
+                if (this.heap[leftIdx] < element) {
+                    swap = leftIdx;
+                }
+            }
+            if (rightIdx < length) {
+                if (
+                    (swap === null && this.heap[rightIdx] < element) ||
+                    (swap !== null && this.heap[rightIdx] < this.heap[leftIdx])
+                ) {
+                    swap = rightIdx;
+                }
+            }
+
+            if (swap === null) break;
+            this.heap[idx] = this.heap[swap];
+            this.heap[swap] = element;
+            idx = swap;
+        }
+    }
 }
 
-module.exports=mincost;
+function mincost(arr) {
+    const heap = new MinHeap();
+
+    for (let rope of arr) {
+        heap.insert(rope);
+    }
+
+    let totalCost = 0;
+
+    while (heap.size() > 1) {
+        const first = heap.extractMin();
+        const second = heap.extractMin();
+        const cost = first + second;
+        totalCost += cost;
+        heap.insert(cost);
+    }
+
+    return totalCost;
+}
